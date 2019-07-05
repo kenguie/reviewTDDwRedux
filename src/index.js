c9d78820eb6b4ca7b197bfe89fb166349423d90b
Ken Guie <nyhunter77@yahoo.com>
2019-07-05 13:41:57 -0400

    store is set up

--- src/index.js
+++ src/index.js
@@ -1,5 +1,15 @@
 import React from 'react';
 import { render } from 'react-dom';
+import { createStore } from 'redux'; // bring in redux
+import { Provider } from 'react-redux'; // this allows the store to be provided to the App
+import rootReducer from './reducers/balance'; // bring in the reducer to affect the store
 import App from './components/App';
 
-render(<App />, document.getElementById('root'));
+const store = createStore(rootReducer);
+
+render(
+  <Provider store={store}>
+    <App />
+  </Provider>, 
+  document.getElementById('root')
+);
