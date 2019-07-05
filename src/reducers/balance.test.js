2189b8872b7fda30672927ee4104d3cf436cb8a6
Ken Guie <nyhunter77@yahoo.com>
2019-07-05 16:04:50 -0400

    added tests and code for deposit and withdrawals

--- src/reducers/balance.test.js
+++ src/reducers/balance.test.js
@@ -7,4 +7,18 @@ describe('balanceReducer', () => {
 
     expect(balanceReducer(undefined, { type: constants.SET_BALANCE, balance })).toEqual(balance); // undefined is the initial val, then replaced by new value
   })
+
+  it('deposits into the balance', () => {
+    const deposit = 10;
+    const initialState = 5;
+
+    expect(balanceReducer(initialState, { type: constants.DEPOSIT, deposit })).toEqual(deposit + initialState);
+  })
+
+  it('withdraws from the balance', () => {
+    const withdraw = 10;
+    const initialState = 25;
+
+    expect(balanceReducer(initialState, { type: constants.WITHDRAW, withdraw })).toEqual(initialState - withdraw);
+  })
 })
